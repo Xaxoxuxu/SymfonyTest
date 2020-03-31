a61e2229a1f358bca009540a77fc19e56243497c
Xaxoxuxu <tonkatapk1@gmail.com>
2020-03-31 18:13:53 +0300

    Added profiler and basic twig page

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController // gives us shorthand methods (render)
 {
     /**
      * @Route("/")
@@ -20,8 +21,17 @@ class QuestionController
     /**
      * @Route("/questions/{slug}")
      */
-    public function show(string $slug)
+    public function show($slug)
     {
-        return new Response("Path: $slug");
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
